cbb0af4f40dd74d15f3fc9bda863cbc3f12bf5c5
Francesco Stella <francesco.stella23@gmail.com>
2014-04-14 15:21:11 -0400

    refactor code in one object wrapper

--- devkit.js
+++ devkit.js
@@ -1,62 +1,74 @@
-var Debug = {};
+/*!
+ *	Devkit-js - https://github.com/francescostella/devkit-js
+ *	Author: Francesco Stella
+ *	Devkit-js may be freely distributed under the MIT license. 
+ *
+ *	To use it: Devkit.log({ 'Label': varName });
+ */
 
-// Creation of 'message' obj
-Debug.messages = {};
+'use strict';
 
-Debug.log = function(elem) {
-	// To log use: Debug.log({ 'Label': varName });
+var Devkit = Devkit || {};
+Devkit.messages = {};
 
+Devkit.log = function(elem) {
+	
 	for (var prop in elem)  {
-		Debug.messages[prop] = elem[prop];
+		Devkit.messages[prop] = elem[prop];
 	}
-			
-	// Create output message
+	
 	var output = "";
 	
-	for (var prop in Debug.messages){
+	for (var prop in Devkit.messages){
 		var line = "";
-		line = line + ("<b>" + prop + "</b>: " + Debug.messages[prop]);
+		line = line + ("<b>" + prop + "</b>: " + Devkit.messages[prop]);
 		output = output + line + "<br>";
 	}
-	
-	// Creation of hud
-	if(!document.getElementById('debug-log-hud')) {
-		var element = document.createElement('div');
-		
-		element.id = 'debug-log-hud';
-		element.style.position = "fixed";
-		element.style.bottom = "20px";
-		element.style.right = "20px";
-		element.style.color = "white";
-		element.style.backgroundColor = "rgba(0,0,0,.75)";
-		element.style.padding = "8px";
-		element.style.borderRadius = "3px";
-		element.style.fontFamily = "Arial, sans-serif";
-		element.style.fontSize = "12px";
-		element.style.lineHeight = "1.3";
-		element.style.webkitFontSmoothing = "antialiased";
-		element.style.zIndex = 99;
 
-		var test = document.getElementById('test')
-		document.lastChild.appendChild(element);
-
-		// Bindings
-		var isPressed = false;
-
-		$(document).keydown(function(e) {
-			console.log('toggle');
-			if(!isPressed) {
-				isPressed = true;
-				if(e.which == 68) {
-					e.preventDefault();
-					$('#debug-log-hud').slideToggle(400,function() {
-						isPressed = false;
-					});
-				}
-			}
-		});
+	if(!document.getElementById('devkit-hud')) { 
+		Devkit.createHud(); 
+		Devkit.devkitHud = document.getElementById('devkit-hud');
 	}
-		
-	// Output
-	$("#debug-log-hud").html(output);
+	
+	Devkit.devkitHud.innerHTML = output;
 };
+
+Devkit.bindings = function () {
+	document.onkeydown = function(e) {	
+		e.preventDefault();
+
+		if(e.which != 27) {
+			return false;
+		}
+
+		if (Devkit.devkitHud.style.display === 'none') {
+			Devkit.devkitHud.style.display = 'block';
+		} else {
+			Devkit.devkitHud.style.display = 'none';
+		}
+	};
+};
+
+Devkit.createHud = function () {		
+	var element = document.createElement('div');
+	
+	element.id = 'devkit-hud';
+	element.style.position = "fixed";
+	element.style.bottom = "20px";
+	element.style.right = "20px";
+	element.style.color = "white";
+	element.style.backgroundColor = "rgba(0,0,0,.75)";
+	element.style.padding = "8px";
+	element.style.borderRadius = "3px";
+	element.style.fontFamily = "Arial, sans-serif";
+	element.style.fontSize = "12px";
+	element.style.lineHeight = "1.3";
+	element.style.webkitFontSmoothing = "antialiased";
+	element.style.zIndex = 99;
+
+	var test = document.getElementById('test')
+	document.lastChild.appendChild(element);
+
+	Devkit.bindings(element);
+};
+
